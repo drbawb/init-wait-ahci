c1ed5797b367208709d5a38d617ff0fa014db027
Robert Straw <drbawb@fatalsyntax.com>
2026-03-05 12:48:49 -0600

    clean up some non-idiomatic crap ...

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
-#![feature(iterator_try_collect)]
-
 use std::{fmt, fs, io};
-use std::sync::{Arc, RwLock};
+use std::sync::{Arc, Mutex};
 use std::thread::{self, JoinHandle};
 use std::time::{Duration, Instant};
 
-const PCI_BUS_PATH: &'static str = "/sys/bus/pci/devices";
+const WAIT_NUM_DEVICES: u8 = 4;
+const WAIT_ATA_DEVICES: u8 = 24;
 
-const PCI_DEV_BID: &'static str = "0000:67:00.0";
-const PCI_DEV_VID: &'static str = "0x1b21";
-const PCI_DEV_DID: &'static str = "0x1164";
+const PCI_BUS_PATH: &str = "/sys/bus/pci/devices";
+
+const PCI_DEV_BID: &str = "0000:67:00.0";
+const PCI_DEV_VID: &str = "0x1b21";
+const PCI_DEV_DID: &str = "0x1164";
 
 #[derive(Debug)]
 struct TaskRegister {
-    devices_found: i8,
+    devices_found: u8,
 }
 
 #[derive(Debug)]
@@ -30,6 +31,10 @@ impl From<io::Error> for CmdErr {
 
 impl From<&str> for CmdErr {
     fn from(err: &str) -> CmdErr { CmdErr::Logic(err.into()) }
+}
+
+impl From<String> for CmdErr {
+    fn from(err: String) -> CmdErr { CmdErr::Logic(err) }
 }
 
 impl fmt::Display for CmdErr {
@@ -96,7 +101,7 @@ fn main() {
     let mut any_error = false;
     let mut handles = Vec::with_capacity(paths.len());
     let start_time = Instant::now();
-    let thread_state = Arc::new(RwLock::new(TaskRegister { devices_found: 0 }));
+    let thread_state = Arc::new(Mutex::new(TaskRegister { devices_found: 0 }));
 
     for path in paths {
         handles.push(wait_for_ata_dev(start_time, path, thread_state.clone()));
@@ -111,10 +116,10 @@ fn main() {
     if any_error { std::process::exit(0x10); }
 
     // check the results from our workers if there were no unexpected errors ...
-    let task_regs = thread_state.read().expect("could not lock task register state");
+    let task_regs = thread_state.lock().expect("could not lock task register state");
 
-    if task_regs.devices_found != 4 {
-        eprintln!("found [{}] devices expected [4]", task_regs.devices_found);
+    if task_regs.devices_found != WAIT_NUM_DEVICES {
+        eprintln!("found [{}] devices expected [{WAIT_NUM_DEVICES}]", task_regs.devices_found);
         std::process::exit(0x01);
     }
 
@@ -122,42 +127,43 @@ fn main() {
     std::process::exit(0x00);
 }
 
-fn wait_for_ata_dev(started_at: Instant, path: String, regs: Arc<RwLock<TaskRegister>>) -> JoinHandle<Result<(), CmdErr>> {
+fn wait_for_ata_dev(started_at: Instant, path: String, regs: Arc<Mutex<TaskRegister>>) -> JoinHandle<Result<(), CmdErr>> {
     thread::spawn(move || {
-        let ata_no: i8 = path
+        let ata_no: u8 = path
             .strip_prefix("ata").ok_or(CmdErr::from("started thread with non ata* path?"))?
             .parse().map_err(|_| CmdErr::from("ata* path does not end with integer?"))?;
 
-        let scsi_no = ata_no - 1;
+        let scsi_no = ata_no.checked_sub(1)
+            .ok_or(CmdErr::from(format!("integer underflow: did not expect ata device [{ata_no}]")))?;
+
         let host_path = format!("{PCI_BUS_PATH}/{PCI_DEV_BID}/ata{ata_no}/host{scsi_no}/target{scsi_no}:0:0/{scsi_no}:0:0:0");
         println!("started thread for {host_path}");
 
         'task: loop {
             if started_at.elapsed() > Duration::from_secs(30) {
-                eprintln!("timeout reached for {host_path}");
-                break 'task; // exit early on timeout ...
+                return Err(CmdErr::from(format!("timeout exceeded for: {path}")))
             }
 
-            let mut task_reg = regs.write().map_err(|_| CmdErr::from("failed to lock task register"))?;
-            if task_reg.devices_found >= 4 { break 'task } // exit early if other threads beat us ...
+            let mut task_reg = regs.lock().map_err(|_| CmdErr::from("failed to lock task register"))?;
+            if task_reg.devices_found >= WAIT_NUM_DEVICES { break 'task } // exit early if other threads beat us ...
 
             if fs::exists(host_path.as_str()).map_err(|_| CmdErr::from("i/o error reading host path"))? {
                 let elapsed_ms = started_at.elapsed().as_millis();
                 task_reg.devices_found += 1;
                 println!("found device at [{host_path}] in {elapsed_ms}ms");
-                break 'task;
+                break 'task // ... or exit early when device is found
             }
 
             drop(task_reg); // lock is otherwise held for scope of loop
             thread::sleep(Duration::from_millis(100));
         }
 
-        return Ok(()); // exit successfully ...
+        Ok(()) // exit successfully ...
     })
 }
 
 fn enumerate_ata_paths(dir_tree: fs::ReadDir) -> Result<Vec<String>, CmdErr> {
-    let mut paths = Vec::with_capacity(24); // assumed that this controller has 24 ports
+    let mut paths = Vec::with_capacity(WAIT_ATA_DEVICES as usize);
 
     for ent in dir_tree {
         let ent = ent?; // assume we are able to read this dnode
@@ -171,5 +177,5 @@ fn enumerate_ata_paths(dir_tree: fs::ReadDir) -> Result<Vec<String>, CmdErr> {
         paths.push(path_name); // found an ATA path
     }
 
-    return Ok(paths);
+    Ok(paths)
 }

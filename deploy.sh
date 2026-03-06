22adebdcbf00fa53b76a5380551bfac28c2a0be7
Robert Straw <drbawb@fatalsyntax.com>
2026-03-06 11:25:08 -0600

    remove obsolete deploy.sh script

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-mv -ft /usr/local/bin target/debug/init-wait-ahci
-chown root:wheel /usr/local/bin/init-wait-ahci
-chmod 750 /usr/local/bin/init-wait-ahci
-
